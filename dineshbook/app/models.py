028773933866600e71471b6ee4951ed7dac93cd4
DINESH1Z <rldc1622@gmail.com>
2023-03-22 19:17:44 +0530

    made migrations

--- dineshbook/app/models.py
+++ dineshbook/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+user = get_user_model()
 # Create your models here.
+class profile(models.Model):
+    user = models.ForeignKey(user, on_delete= models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to= 'profile_ images', 
+    default= 'blank-profile-picture.png')
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
